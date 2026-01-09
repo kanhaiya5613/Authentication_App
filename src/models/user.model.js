db587f708937133dbac6b54435349cee257c2c2a
kanhaiya kumar <kk7077574@gmail.com>
2026-01-09 15:20:40 +0530

    Created User Model and Delete and Upload File to Cloudinary

---
+++ src/models/user.model.js
@@ -0,0 +1,92 @@
+import mongoose,{Schema} from "mongoose";
+import jwt from "jsonWebToken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        userName:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullName: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            url: {
+                type: String,
+                required: true
+            },
+            public_id: {
+                type: String,
+                required: true
+            }
+        },
+        coverImage: {
+            url:{
+                type:String
+            },
+            public_id:{
+                type:String
+            }
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is Required']
+        },
+        refreshToken:{
+            type: String
+        }
+    }
+)
+
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("Password")) return;
+    this.password = bcrypt.hash(this.password,10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        __id : this.__id,
+        email: this.email,
+        userName: this.userName,
+        fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    },
+    
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+    
+)
+}
---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,35 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+
+// Configuration 
+ cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD-NamedNodeMap,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+ });
+
+ const uploadOnCloudinary = async(localFilePath) => {
+    try{
+        if(!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath,{resource_type: "auto"});
+        fs.unlinkSync(localFilePath)
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+ }
+ // Delete From Cloudinary
+ const deleteFromCloudinary = async (publicId) => {
+    try{
+        if(!publicId) return null;
+
+        const response = await cloudinary.uploader.destroy(publicId);
+        return response;
+    }catch(error){
+        console.error('Cloudinary Delete Error:', error);
+        return null;
+    }
+ };
+
+ export {uploadOnCloudinary, deleteFromCloudinary}
